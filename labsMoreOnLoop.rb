7784cb85a090a1a2c35f00fa69252af74c477d30
wardahmad <whitelight43@hotmail.com>
2019-12-09 16:22:52 +0300

    addLabsMoreOnLoop.rb Unfinshed

---
+++ labsMoreOnLoop.rb
@@ -0,0 +1,116 @@
+choose the correct answer.
+While loop checks the condition and the loop keeps on running till the condition is true, it stops when the condition becomes false.
+a) True b) False
+_____True
+
+What is the output of the given code?
+counter = 1
+while counter < 11
+  puts counter
+  counter = counter + 1
+end
+Prints the number from 1 to 10
+Prints the number from 1 to 11
+Prints the number from 2 to 10
+Infinite loop
+What is the output of the given code?
+counter = true
+while counter !=false
+  puts counter
+end
+True
+False
+Syntax error
+Infinite loop
+What is the output of the given code?
+i = 0
+while i < 5
+  puts i
+   i=(i+1)**2
+end
+1 2 3 4 5
+0 1 4
+0 1
+1 4
+What is the output of the given code?
+a=5
+b=15
+while a&&b
+puts a+b
+end
+5..15
+20
+Infinite loop
+5 15
+What is the output of the given code?
+a=5
+b=15
+while b>a
+puts a*(b-a)
+while a>b
+  a+=1
+  b-=1
+end
+end
+5..15
+20
+Infinite loop
+5 15
+What is the output of the given code?
+i = 3
+while i > 0 do
+  print i
+  i -= 1
+end
+3
+321
+Infinite loop
+3 2 1 0
+What is the output of the given code?
+i = 50
+while i > 25 do
+  print 50/i
+  i -= 1
+end
+50..25
+50..1
+Infinite loop
+1111111111111111111111111
+What is the output of the given code?
+a = 5
+b=10
+while a<b do
+  puts a*b
+  a+=2
+  b-=2
+  end
+5 10
+50 56
+Infinite loop
+5 6 7 8 9 10
+What is the output of the given code?
+i = 50 
+j=55
+while i > 25 && j>35 do
+  puts 50*j/i
+  i -= 1
+  j-=2
+end
+25 35
+50 55
+Infinite loop
+55 54 53 52 51 50 48 47 46 45
+What is the output of the given code?
+i = 50 
+j=55
+while i > 25 && i*j<100 do
+  puts (50*j)/i
+  i -= 1
+  j-=2
+end
+25 35
+No output
+Infinite loop
+55 54 53 52 51 50 48 47 46 45
+Take inputs from user to make an array. Again take one input from user and search it in the array and delete that element, if found. Iterate over array using for loop.
+Using (1...101), make two array, one containing all even numbers and other containing all odd numbers.
